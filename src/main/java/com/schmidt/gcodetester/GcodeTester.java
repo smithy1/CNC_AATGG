18512887fb609e8f752036baded72383e7fd78b6
Schmidt Zoltán <schmidt.zoltan@me.com>
2023-02-07 12:32:57 +0100

    Initial commit

---
+++ src/main/java/com/schmidt/gcodetester/GcodeTester.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.schmidt.gcodetester;
+
+/**
+ *
+ * @author Zoli
+ */
+public class GcodeTester {
+
+    public static void main(String[] args) {
+
+        new GcodeFrame().setVisible(true);
+
+    }
+}
